a8ef61f3bc4121c3c410e09fcaccfd29a45bf7df
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2016-07-22 13:38:21 -0400

    added basic WordPress files

---
+++ footer.php
@@ -0,0 +1,13 @@
+ <hr>
+
+      <footer>
+        <p>&copy; 2015 Company, Inc.</p>
+      </footer>
+    </div> <!-- /container -->
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
